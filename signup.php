c208c103e0f810642de073938d61cce1eb831676
oswpadron <oswaldopadron99@gmail.com>
2025-01-25 12:20:39 -0400

    feat: new styles

--- signup.php
+++ signup.php
@@ -13,15 +13,18 @@ include('header.php');
 <title>Sign Up</title>
 </head>
 <body>
-<form method="post" action="./signup_logica.php">
-<label id="nombre">Nombre de usuario: <input id="nombre" name="nombre"
+    <div class="center-elements">
+<h1>Regístrate aquí</h1>
+<form class="signup-form" method="post" action="./signup_logica.php">
+<label id="nombre">Nombre de usuario: <input class="signup-input" id="nombre" name="nombre"
 type="text" required /></label>
-<label id="password">Contraseña: <input id="password" name="password"
+<label id="password">Contraseña: <input class="signup-input" id="password" name="password"
 type="password" required /></label>
 </select>
 </label>
-<input id="registrar" name="Registrar" type="submit" />
-<input id="limpiar" name="Limpiar" type="reset" />
+<input id="registrar" name="Registrar" type="submit" class="signup-input" />
+<input id="limpiar" name="Limpiar" type="reset" class="signup-input" />
 </form>
+</div>
 </body>
 </html>
